6c85682c062ee9536488d4db886fd9408709b2db
Tina Liang <tinaliang129@gmail.com>
2024-08-19 15:19:03 -0700

    unit test setup

--- src/test/java/com/revature/planetarium/unitTests/repository/PlanetDaoImpUnitTests.java
+++ src/test/java/com/revature/planetarium/unitTests/repository/PlanetDaoImpUnitTests.java
@@ -3,23 +3,43 @@ package com.revature.planetarium.unitTests.repository;
 import com.revature.planetarium.entities.Moon;
 import com.revature.planetarium.entities.Planet;
 import com.revature.planetarium.entities.User;
+import com.revature.planetarium.repository.planet.PlanetDaoImp;
+import com.revature.planetarium.utility.DatabaseConnector;
 import org.junit.*;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PlanetDaoImpUnitTests {
     private User user;
-
     private Planet planet;
-
     private Moon moon;
+
+    private PlanetDaoImp planetDaoImp;
+    private Connection mockConnections;
+    private PreparedStatement mockPrepStmt;
+    private ResultSet mockRstSet;
+    private static MockedStatic<DatabaseConnector> mockedStatic;
+
 
     @BeforeClass
     public static void setup(){
-
+        mockedStatic = Mockito.<DatabaseConnector>mockStatic(DatabaseConnector.class);
     }
 
     @Before
-    public void beforeEach(){
+    public void beforeEach() throws SQLException {
+        planetDaoImp = new PlanetDaoImp();
 
+        mockConnections = Mockito.mock(Connection.class);
+        mockPrepStmt = Mockito.mock(PreparedStatement.class);
+        mockRstSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(DatabaseConnector.getConnection()).thenReturn(mockConnections);
     }
 
     @After
