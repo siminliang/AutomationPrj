beda41aa70f211f1b7eef5932ab518540c8fcabe
isaias865 <isaias865@revature.net>
2024-08-28 10:39:37 -0500

    Created unit and integration tests for moon service and planet service

--- src/test/java/com/revature/planetarium/integrationTests/service/MoonServiceImpIntegrationTests.java
+++ src/test/java/com/revature/planetarium/integrationTests/service/MoonServiceImpIntegrationTests.java
@@ -1,39 +1,226 @@
 package com.revature.planetarium.integrationTests.service;
 
+import com.revature.planetarium.Utility;
 import com.revature.planetarium.entities.Moon;
-import com.revature.planetarium.entities.Planet;
-import com.revature.planetarium.entities.User;
+import com.revature.planetarium.exceptions.MoonFail;
+import com.revature.planetarium.repository.moon.MoonDaoImp;
+import com.revature.planetarium.service.moon.MoonServiceImp;
 import org.junit.*;
 
+import java.io.Serializable;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class MoonServiceImpIntegrationTests {
-    private User user;
-
-    private Planet planet;
-
-    private Moon moon;
-
-    @BeforeClass
-    public static void setup(){
-
-    }
+    private MoonDaoImp moonDao;
+    private MoonServiceImp<Serializable> moonService;
 
     @Before
     public void beforeEach(){
-
-    }
-
-    @After
-    public void afterEach(){
-
+        moonDao = new MoonDaoImp();
+        moonService = new MoonServiceImp<>(moonDao);
+        Utility.resetTestDatabase();
     }
 
     @AfterClass
-    public static void tearDown(){
+    public static void tearDown(){ Utility.resetTestDatabase(); }
 
+    @Test
+    public void testCreateMoon_Positive(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon test = moonService.createMoon(moon);
+
+        assertEquals("Phobos", test.getMoonName());
     }
 
     @Test
-    public void testExamplePlaceHolder(){
+    public void testCreateMoon_NameTooShort_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("");
+        moon.setOwnerId(1);
 
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon));
+        assertEquals("Moon name must be between 1 and 30 characters", moonFail.getMessage());
+    }
+
+    @Test
+    public void testCreateMoon_NameTooLong_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("this moon name is 31 chars!!!!!");
+        moon.setOwnerId(1);
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon));
+        assertEquals("Moon name must be between 1 and 30 characters", moonFail.getMessage());
+    }
+
+    @Test
+    public void testCreateMoon_NonUniqueName_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        moonService.createMoon(moon);
+
+        Moon moon2 = new Moon();
+        moon2.setMoonName("Phobos");
+        moon2.setOwnerId(2);
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon2));
+        assertEquals("Moon name must be unique", moonFail.getMessage());
+    }
+
+    @Test
+    public void testSelectMoon_ByName_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        moonService.createMoon(moon);
+
+        Moon test = moonService.selectMoon("Phobos");
+
+        assertEquals("Phobos", test.getMoonName());
+    }
+
+    @Test
+    public void testSelectMoon_ById_Positive(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon createdMoon = moonService.createMoon(moon);
+
+        Moon test = moonService.selectMoon(createdMoon.getMoonId());
+
+        assertEquals(createdMoon.getMoonId(), test.getMoonId());
+    }
+
+    @Test
+    public void testSelectMoon_Negative() {
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.selectMoon("This moon does not exists"));
+        assertEquals("Moon not found", moonFail.getMessage());
+    }
+
+    @Test
+    public void testSelectAllMoons_Positive(){
+        List<Moon> moons = moonService.selectAllMoons();
+        assertFalse(moons.isEmpty());
+    }
+
+    @Test
+    public void testSelectAllMoons_Negative(){
+        moonDao.deleteMoon(1);
+        moonDao.deleteMoon(2);
+
+        List<Moon> moons = moonService.selectAllMoons();
+        assertTrue(moons.isEmpty());
+    }
+
+    @Test
+    public void testSelectByPlanet_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        moonService.createMoon(moon);
+
+        List<Moon> moons = moonService.selectByPlanet(1);
+
+        assertEquals(2, moons.size());
+    }
+
+    @Test
+    public void testSelectByPlanet_Negative(){
+        List<Moon> moons = moonService.selectByPlanet(99);
+
+        assertTrue(moons.isEmpty());
+    }
+
+    @Test
+    public void testUpdateMoon_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon moon2 = moonService.createMoon(moon);
+
+        moon2.setMoonName("Callisto");
+
+        Moon test = moonService.updateMoon(moon2);
+
+        assertEquals("Callisto", test.getMoonName());
+    }
+
+    @Test
+    public void testUpdateMoon_MoonNotFound_Negative() {
+        Moon moon = new Moon();
+        moon.setMoonId(9999);
+        moon.setMoonName("Phobos");
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(moon));
+        assertEquals("Moon not found, could not update", moonFail.getMessage());
+    }
+
+    @Test
+    public void testUpdatePlanet_NameTooShort_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon updatedMoon = moonService.createMoon(moon);
+
+        updatedMoon.setMoonName("");
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(updatedMoon));
+        assertEquals("Moon name must be between 1 and 30 characters, could not update", moonFail.getMessage());
+    }
+
+    @Test
+    public void testUpdatePlanet_NameTooLong_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon updatedMoon = moonService.createMoon(moon);
+
+        updatedMoon.setMoonName("this moon name is 31 chars!!!!!");
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(updatedMoon));
+        assertEquals("Moon name must be between 1 and 30 characters, could not update", moonFail.getMessage());
+    }
+
+    @Test
+    public void testDeleteMoon_ById_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        Moon createdMoon = moonService.createMoon(moon);
+
+        String result = moonService.deleteMoon(createdMoon.getMoonId());
+        assertEquals("Moon deleted successfully", result);
+    }
+
+    @Test
+    public void testDeleteMoon_ByName_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        moonService.createMoon(moon);
+
+        String result = moonService.deleteMoon("Phobos");
+
+        assertEquals("Moon deleted successfully", result);
+    }
+
+    @Test
+    public void testDeleteMoon_Negative(){
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.deleteMoon(999));  // Assuming this ID does not exist
+        assertEquals("Moon delete failed, please try again", moonFail.getMessage());
     }
 }
--- src/test/java/com/revature/planetarium/unitTests/service/MoonServiceImpUnitTests.java
+++ src/test/java/com/revature/planetarium/unitTests/service/MoonServiceImpUnitTests.java
@@ -1,16 +1,25 @@
 package com.revature.planetarium.unitTests.service;
 
 import com.revature.planetarium.entities.Moon;
-import com.revature.planetarium.entities.Planet;
-import com.revature.planetarium.entities.User;
+import com.revature.planetarium.exceptions.MoonFail;
+import com.revature.planetarium.repository.moon.MoonDao;
+import com.revature.planetarium.service.moon.MoonServiceImp;
 import org.junit.*;
+import org.mockito.Mockito;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class MoonServiceImpUnitTests {
-    private User user;
 
-    private Planet planet;
-
-    private Moon moon;
+    private MoonDao moonDao;
+    private MoonServiceImp<Serializable> moonService;
 
     @BeforeClass
     public static void setup(){
@@ -19,7 +28,8 @@ public class MoonServiceImpUnitTests {
 
     @Before
     public void beforeEach(){
-
+        moonDao = Mockito.mock(MoonDao.class);
+        moonService = new MoonServiceImp<>(moonDao);
     }
 
     @After
@@ -33,7 +43,211 @@ public class MoonServiceImpUnitTests {
     }
 
     @Test
-    public void testExamplePlaceHolder(){
+    public void testCreateMoon_Positive(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
 
+        when(moonDao.readMoon("Phobos")).thenReturn(Optional.empty());
+        when(moonDao.createMoon(moon)).thenReturn(Optional.of(moon));
+
+        Moon test = moonService.createMoon(moon);
+
+        assertEquals("Phobos", test.getMoonName());
+    }
+
+    @Test
+    public void testCreateMoon_NameTooShort_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("");
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon));
+        assertEquals("Moon name must be between 1 and 30 characters", moonFail.getMessage());
+    }
+
+    @Test
+    public void testCreateMoon_NameTooLong_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("this moon name is 31 chars!!!!!");
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon));
+        assertEquals("Moon name must be between 1 and 30 characters", moonFail.getMessage());
+    }
+
+    @Test
+    public void testCreateMoon_NonUniqueName_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+
+        when(moonDao.readMoon("Phobos")).thenReturn(Optional.of(moon));
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.createMoon(moon));
+        assertEquals("Moon name must be unique", moonFail.getMessage());
+    }
+
+    @Test
+    public void testSelectMoon_ByName_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+
+        when(moonDao.readMoon("Phobos")).thenReturn(Optional.of(moon));
+
+        Moon selectedMoon = moonService.selectMoon("Phobos");
+
+        assertEquals("Phobos", selectedMoon.getMoonName());
+    }
+
+    @Test
+    public void testSelectMoon_ById_Positive(){
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setMoonId(1);
+
+        when(moonDao.readMoon(1)).thenReturn(Optional.of(moon));
+
+        Moon test = moonService.selectMoon(1);
+
+        assertEquals(1,test.getMoonId());
+    }
+
+    @Test
+    public void testSelectMoon_Negative() {
+        when(moonDao.readMoon("This moon does not exists")).thenReturn(Optional.empty());
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.selectMoon("This moon does not exists"));
+        assertEquals("Moon not found", moonFail.getMessage());
+    }
+
+    @Test
+    public void testSelectAllMoons_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        Moon moon2 = new Moon();
+        moon2.setMoonName("Titan");
+
+        when(moonDao.readAllMoons()).thenReturn(Arrays.asList(moon, moon2));
+
+        List<Moon> moons = moonService.selectAllMoons();
+
+        assertEquals(2, moons.size());
+    }
+
+    @Test
+    public void testSelectAllMoons_Negative(){
+        when(moonDao.readAllMoons()).thenReturn(Collections.emptyList());
+
+        List<Moon> test = moonService.selectAllMoons();
+
+        assertTrue(test.isEmpty());
+    }
+
+    @Test
+    public void testSelectByPlanet_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setOwnerId(1);
+
+        when(moonDao.readMoonsByPlanet(1)).thenReturn(Arrays.asList(moon));
+
+        List<Moon> moons = moonService.selectByPlanet(1);
+
+        assertEquals("Phobos", moons.get(0).getMoonName());
+    }
+
+    @Test
+    public void testSelectByPlanet_Negative(){
+        when(moonDao.readMoonsByPlanet(1)).thenReturn(Collections.emptyList());
+
+        List<Moon> test = moonService.selectByPlanet(1);
+
+        assertTrue(test.isEmpty());
+    }
+
+    @Test
+    public void testUpdateMoon_Positive() {
+        Moon moon = new Moon();
+        moon.setMoonName("Phobos");
+        moon.setMoonId(1);
+
+        Moon updatedMoon = new Moon();
+        updatedMoon.setMoonName("Titan");
+        updatedMoon.setMoonId(1);
+
+        when(moonDao.readMoon(1)).thenReturn(Optional.of(moon));
+        when(moonDao.readMoon("Titan")).thenReturn(Optional.empty());
+        when(moonDao.updateMoon(updatedMoon)).thenReturn(Optional.of(updatedMoon));
+
+        Moon test = moonService.updateMoon(updatedMoon);
+
+        assertEquals("Titan", test.getMoonName());
+    }
+
+    @Test
+    public void testUpdateMoon_MoonNotFound_Negative() {
+        Moon moon = new Moon();
+        moon.setMoonId(1);
+        moon.setMoonName("Phobos");
+
+        when(moonDao.readMoon(1)).thenReturn(Optional.empty());
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(moon));
+        assertEquals("Moon not found, could not update", moonFail.getMessage());
+    }
+
+    @Test
+    public void testUpdatePlanet_NameTooShort_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonId(1);
+        moon.setMoonName("Phobos");
+
+        Moon updatedMoon = new Moon();
+        updatedMoon.setMoonName("");
+        updatedMoon.setMoonId(1);
+
+        when(moonDao.readMoon(1)).thenReturn(Optional.of(moon));
+
+        MoonFail planetFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(updatedMoon));
+        assertEquals("Moon name must be between 1 and 30 characters, could not update", planetFail.getMessage());
+    }
+
+    @Test
+    public void testUpdatePlanet_NameTooLong_Negative(){
+        Moon moon = new Moon();
+        moon.setMoonId(1);
+        moon.setMoonName("Phobos");
+
+        Moon updatedMoon = new Moon();
+        updatedMoon.setMoonName("this moon name is 31 chars!!!!!");
+        updatedMoon.setMoonId(1);
+
+        when(moonDao.readMoon(1)).thenReturn(Optional.of(moon));
+
+        MoonFail planetFail = assertThrows(MoonFail.class, () -> moonService.updateMoon(updatedMoon));
+        assertEquals("Moon name must be between 1 and 30 characters, could not update", planetFail.getMessage());
+    }
+
+    @Test
+    public void testDeleteMoon_ById_Positive() {
+        when(moonDao.deleteMoon(1)).thenReturn(true);
+
+        String test = moonService.deleteMoon(1);
+
+        assertEquals("Moon deleted successfully", test);
+    }
+
+    @Test
+    public void testDeleteMoon_ByName_Positive() {
+        when(moonDao.deleteMoon("Phobos")).thenReturn(true);
+
+        String test = moonService.deleteMoon("Phobos");
+
+        assertEquals("Moon deleted successfully", test);
+    }
+
+    @Test
+    public void testDeleteMoon_Negative(){
+        when(moonDao.deleteMoon(1)).thenReturn(false);
+
+        MoonFail moonFail = assertThrows(MoonFail.class, () -> moonService.deleteMoon(1));
+        assertEquals("Moon delete failed, please try again", moonFail.getMessage());
     }
 }
